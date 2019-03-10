e2c7d2d2418c6b03b41663b0305fe3284ebb411e
Joseph Holley <joeholley4@gmail.com>
2019-03-09 22:53:04 -0500

    debug logging

--- internal/mmf/mmf.go
+++ internal/mmf/mmf.go
@@ -47,6 +47,7 @@ func makeMatches(profile string, rosters []*api.Roster, pools []*api.PlayerPool)
 					mmfLog.Info("   Looking for player in pool: ", pool.Name, len(pool.Roster.Players))
 					// Get a random index (subtract 1 because arrays are zero-indexed)
 					randPlayerIndex := rand.New(rand.NewSource(time.Now().UnixNano())).Intn(len(pool.Roster.Players)) - 1
+					mmfLog.Infof("   Selected player index %v", randPlayerIndex)
 					selectedPlayer := pool.Roster.Players[randPlayerIndex]
 					mmfLog.Infof("   Selected player index %v: %v", randPlayerIndex, selectedPlayer.Id)
 					// Remove this player from the array.
